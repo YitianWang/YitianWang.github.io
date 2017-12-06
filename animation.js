64219aee90c496037c60bffb9370164d5686d638
YitianW <YitianWang@users.noreply.github.com>
2017-12-06 16:36:00 -0500

    update

--- animation.js
+++ animation.js
@@ -1,8 +1,5 @@
  $(document).ready(function(){
  	
-	$("#paper-crane").delay(4000).animate({marginLeft: "+=1500px"}, 5000); 
-	$("#fadeout").delay(4500).fadeOut(2500);
-	$("#fadein").delay(4000).fadeIn(3000);
-
+ 	
 
 });
